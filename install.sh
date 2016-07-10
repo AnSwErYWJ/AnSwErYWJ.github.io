4c67520043b311ceea0fd5ce5f57e9fbc933a6c7
AnSwErYWJ <ywj1993@yeah.net>
2016-07-10 13:28:44 +0800

    next

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install
+npm install -g hexo-cli
+npm install hexo-generator-index --save
+npm install hexo-generator-archive --save
+npm install hexo-generator-category --save
+npm install hexo-generator-tag --save
+npm install hexo-server --save
+npm install hexo-deployer-git --save
+npm install hexo-deployer-heroku --save
+npm install hexo-deployer-rsync --save
+npm install hexo-deployer-openshift --save
+npm install hexo-renderer-marked --save
+npm install hexo-renderer-stylus --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-sitemap --save
+
+# rss feed
+npm install hexo-generator-feed --save
+
+# search
+npm install hexo-generator-search --save
