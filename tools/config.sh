f08aa176b0507f54c212a9158886329d8cf7a7a9
answerywj <1045837697@qq.com>
2023-02-18 12:05:14 +0800

    添加容器环境配置脚本

---
+++ tools/config.sh
@@ -0,0 +1,10 @@
+###
+ # @FilePath: config.sh
+ # @Author: answerywj
+ # @Mail: yuanweijie1993@gmail.com
+ # @Date: 2023-02-18 12:00:43
+### 
+
+chmod 600 /root/.ssh/*
+git config --global user.name "answerywj"
+git config --global user.email "1045837697@qq.com" 
